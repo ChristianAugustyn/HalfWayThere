8255111e1db5e1dce6fbae70ed4744f69bf40c6b
Christian <chrisaug@rogers.com>
2020-03-20 18:08:08 -0400

    device position is active and displayed using google maps api

--- app/src/main/java/com/example/christian_augustyn_aandroid_developer_halfwaythere/SearchPOI.java
+++ app/src/main/java/com/example/christian_augustyn_aandroid_developer_halfwaythere/SearchPOI.java
@@ -1,7 +1,13 @@
 package com.example.christian_augustyn_aandroid_developer_halfwaythere;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -11,9 +17,17 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class SearchPOI extends FragmentActivity implements OnMapReadyCallback {
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.text.TextUtils;
+
+public class SearchPOI extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
+    private LocationManager lm;
+    private LocationListener llistner;
+    double lat, lng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +37,26 @@ public class SearchPOI extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE); //
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Criteria criteria = new Criteria();
+        String bestProvider = String.valueOf(lm.getBestProvider(criteria, true)).toString();
+        Location location = lm.getLastKnownLocation(lm.GPS_PROVIDER);
+        if (location != null) {
+            onLocationChanged(location);
+
+        } else {
+            lm.requestLocationUpdates(bestProvider, 2000, 0, SearchPOI.this);
+        }
     }
 
 
@@ -38,10 +72,29 @@ public class SearchPOI extends FragmentActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        LatLng userpos = new LatLng(lat, lng);
+        mMap.addMarker(new MarkerOptions().position(userpos).title("Your Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(userpos));
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    @Override
+    public void onLocationChanged(Location location) {
+        lng = location.getLongitude();
+        lat = location.getLatitude();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
